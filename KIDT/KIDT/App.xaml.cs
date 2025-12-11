621ff64671b7fd9d55d7b3ec09c5b536971b2184
Paul Schmidt <Paul.Schmidtwds@gmail.com>
2025-12-11 16:33:47 +0100

    Projektstruktur initialisiert
    Projektstruktur initialisiert#

---
+++ KIDT/KIDT/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace KIDT
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
